48bf7aaa65cec93908176fa7e4883d8adf5f6f15
John Ankcorn <john.ankcorn@qrclab.com>
2014-06-30 16:55:31 -0400

    first shift out from send_data_frame() has to be only 1 bit.
    (since the databits are re-combined in the read result, it is annoying to
    have other junk shifted in as a result of the TMS shifts)

--- src/fpgajtag.c
+++ src/fpgajtag.c
@@ -806,14 +806,10 @@ static void process_command_list(struct ftdi_context *ftdi)
             write_item(DITEM(IDLE_TO_SHIFT_DR));
             for (i = 0; i < len; i++)
                 tempbuf2[i] = tempbuf[len-1-i];
-            send_data_frame(ftdi, DWRITE | DREAD,
-                (found_cortex)
-                  ? DITEM(SHIFT_TO_PAUSE(DREAD, 0),
-                          SHIFT_TO_PAUSE(0,0x80&tempbuf2[len-1]),
-                          //SHIFT_TO_EXIT1(0, 0x80 & tempbuf2[len-1]))
-                          SHIFT_TO_EXIT1(0, 0))
-                  : DITEM(SHIFT_TO_EXIT1(DREAD, 0)),
+            send_data_frame(ftdi, DWRITE | DREAD, DITEM(SHIFT_TO_EXIT1(DREAD, 0)),
                 tempbuf2, len, SEND_SINGLE_FRAME);
+            if (found_cortex)
+                 write_item(DITEM(TMSW, 0x03, 0x0a));
             write_item(DITEM(EXIT1_TO_IDLE));
             uint8_t *rdata = read_data(__LINE__, ftdi, len);
             int i = 0;

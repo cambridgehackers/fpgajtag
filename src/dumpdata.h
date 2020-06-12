271d9b162732d0f14f9c0707d89c4d8d093412b5
John Ankcorn <jca@alum.mit.edu>
2020-06-12 10:25:09 -0700

    improved detail of data tracing, including jtag FSM state display

--- src/dumpdata.h
+++ src/dumpdata.h
@@ -21,6 +21,48 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+enum {
+    state_unknown,
+    state_reset,
+    state_idle,
+    state_selectd,
+    state_capture,
+    state_shift,
+    state_exit1,
+    state_pause,
+    state_exit2,
+    state_update,
+    state_selecti};
+
+static const char *stateName[] = {
+    "",
+    " reset ",
+    " idle ",
+    " selectd ",
+    " capture%s ",
+    " shift%s ",
+    " exit1%s ",
+    " pause%s ",
+    " exit2%s ",
+    " update%s ",
+    " selecti "};
+
+//0   1
+static int nextState[][2] = {
+    {state_unknown, state_unknown}, //state_unknown
+    {state_idle, state_reset}, //state_reset
+    {state_idle, state_selectd}, //state_idle
+    {state_capture, state_selecti}, //state_selectd
+    {state_shift, state_exit1}, //state_capture
+    {state_shift, state_exit1}, //state_shift
+    {state_pause, state_update}, //state_exit1
+    {state_pause, state_exit2}, //state_pause
+    {state_shift, state_update}, //state_exit2
+    {state_idle, state_selectd}, //state_update
+    {state_capture, state_reset}}; //state_selecti
+static int currentState;
+const char *selectType = "";
+
 static void memdumpfile(const unsigned char *p, int len, const char *title)
 {
 int i;
@@ -56,22 +98,75 @@ static void formatwrite(int submit, const unsigned char *p, int len, const char 
         const unsigned char *pstart = p;
         int plen = 1;
         unsigned char ch = *p;
+#define GET_LEN() ((*(p+1) + (*(p+2) << 8))+1)
+        int dataLen = 0;
+        int writeTDI = (ch & 0x10);
+        int writeTMS = (ch & 0x40);
+        printf("COMMAND %02x", ch);
+        if (!(ch & 0x80)) {  // Data shifting command
+            if (ch & 0x2) { // bit mode
+               dataLen = *(p+1) + 1;
+               plen++;
+               printf(": bit datalen %d: ", dataLen);
+               if (writeTDI || writeTMS) { // write
+                   int cdata = *(p+2);
+                   if (writeTMS)
+                       printf("TDI %x TMS ", cdata >> 7);
+                   for (int i = 0; i < dataLen; i++) {
+                       int data = cdata & 1;
+                       printf("%x", data);
+                       cdata >>= 1;
+                       currentState = nextState[currentState][data];
+                       if (writeTMS) {
+                           if (currentState == state_selectd)
+                               selectType = "D";
+                           else if (currentState == state_selecti)
+                               selectType = "I";
+                       }
+                   }
+                   if (writeTMS && dataLen == 5 && (*(p+2) & 0x1f) == 0x1f)
+                       currentState = state_reset;
+                   if (writeTMS)
+                       printf(stateName[currentState], selectType);
+                   plen++;
+               }
+               printf("\n");
+            }
+            else { // byte mode
+               dataLen = GET_LEN();
+               plen += 2;
+               printf(": byte datalen %d%s%s", dataLen, writeTDI != 0 ? " TDI" : "", writeTMS != 0 ? " TMS" : "");
+               if (writeTDI || writeTMS) { // write
+                   plen += dataLen;
+                   memdump(p+3, dataLen, "");
+               }
+               else
+                   printf("\n");
+            }
+        }
+        else
         switch(ch) {
-        case 0x85: case 0x87: case 0x8a: case 0xaa: case 0xab:
-            break;
-        case 0x2e:
-            plen = 2;
-            break;
-        case 0x19: case 0x1b: case 0x2c: case 0x3d: case 0x3f: case 0x4b:
-        case 0x6f: case 0x80: case 0x82: case 0x86: case 0x8f:
+        case 0x80: case 0x82: case 0x86: case 0x8f:
+        case 0x91: case 0x92:
             plen = 3;
+            memdump(p+1, 2, "");
+            break;
+        case 0x93:
+            dataLen = GET_LEN();
+            plen = 3; // bytelen
+            printf(" bytelen %d\n", dataLen);
+            break;
+        case 0x81: case 0x83: case 0x84: case 0x85: case 0x87:
+        case 0x88: case 0x89: case 0x90: case 0xfa:
+        case 0x8a: case 0xaa: case 0xab:
+            plen = 1;
+            printf("\n");
             break;
         default:
-            memdumpfile(p-1, len, title);
-            return;
+            printf("[%s:%d] UNKNOWNCOMMM %x\n", __FUNCTION__, __LINE__, ch);
+            exit(-1);
+            break;
         }
-        //if (!submit || accum < ACCUM_LIMIT)
-            memdumpfile(pstart, plen, header);
         if (started && p[0] == 0x1b && p[1] == 6)
             write(datafile_fd, &bitswap[p[2]], 1);
         p += plen;
@@ -83,10 +178,7 @@ if (tlen > 1500) {
 }
 else
     started = 0;    // shutdown before final writes
-            if (!started)
-                memdumpfile(p, tlen, header_data);
-            //if (submit && tlen > 4) 
-            else {
+            if (started) {
                 int i;
                 for (i = 0; once && i < sizeof(bitswap); i++)
                     bitswap[i] = ((i &    1) << 7) | ((i &    2) << 5)
